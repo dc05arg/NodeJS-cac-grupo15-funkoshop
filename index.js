15837a518dce01d6e112e8b50a8d99266f6cccc4
Diego <dhcavalli@hotmail.com>
2023-12-10 18:51:35 -0300

    Se salva el index.js y se agrega

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Creamos el servidor de archivos estáticos
+const express = require('express');
 
+// Creamos una app
+const app = express();
+
+const PORT = 3008;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
