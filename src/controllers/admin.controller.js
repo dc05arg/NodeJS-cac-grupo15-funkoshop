6e9d7572aac01e3b3e644404c245daed0e18b01e
Diego <dhcavalli@hotmail.com>
2023-12-11 18:28:09 -0300

    se crea admin controller y modifica admin routes

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,10 @@
+// Creamos constantes para luego exportarlas - otra manera de hacerlo con respecto a main.controller.js
+
+const admin = (req, res) => res.send('Vista del ADMIN');
+const createItem = (req, res) => res.send('Vista para CREAR un NUEVO ITEM');
+const addItem = (req, res) => res.send('Ruta para AGREGAR un NUEVO ITEM en la BASE DE DATOS');
+const edit = (req, res) => res.send('Vista para EDITAR un ITEM SELECCIONADO');
+const modify = (req, res) => res.send('Ruta y vista para MODIFICAR PRODUCTO del EDIT');
+const erase = (req, res) => res.send('Ruta y vista para ELIMINAR un ITEM SELECCIONADO');
+
+module.export = { admin, createItem, addItem, edit, modify, erase };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,12 +3,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Vista del ADMIN'));
-router.get('/create', (req, res) => res.send('Vista para CREAR un NUEVO ITEM'));
-router.post('/create', (req, res) => res.send('Ruta para AGREGAR un NUEVO ITEM en la BASE DE DATOS'));
-router.get('/edit/:id', (req, res) => res.send('Vista para EDITAR un ITEM SELECCIONADO'));
-router.put('/edit/:id', (req, res) => res.send('Ruta y vista para MODIFICAR PRODUCTO del EDIT '));
-router.delete('/delete/:id', (req, res) => res.send('Ruta y vista para ELIMINAR un ITEM SELECCIONADO'));
+
+// Importamos el controlador main.controller.js y agregamos ruta al router.get
+
+const controller = requiere('../controllers/admin.controller');
+
+router.get('/', controller.admin);
+router.get('/create', controller.createItem);
+router.post('/create', controller.addItem);
+router.get('/edit/:id', controller.edit);
+router.put('/edit/:id', controller.modify);
+router.delete('/delete/:id', controller.erase);
 
 
 // Exportación
