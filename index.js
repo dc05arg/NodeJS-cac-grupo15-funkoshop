1eb8314bc385799ccabbf004776b780c0d278657
maxiderecho <maxi.derecho@gmail.com>
2023-12-17 13:18:12 -0300

    Migración de código a Node

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 // Creamos el servidor de archivos estáticos
-const express = require('express');
+const express = require("express");
 
 // Creamos una app
 const app = express();
 
 // method-override permite capturar cualquier petición que no sea ademas de post o get
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 // Definición de rutas
 // se hace en carpeta Routes y acá las llamamos
 // --------------
 
-// Routes Imports 
-const mainRoutes = require('./src/routes/main.routes');
-const adminRoutes = require('./src/routes/admin.routes');
-const authRoutes = require('./src/routes/auth.routes');
-const shopRoutes = require('./src/routes/shop.routes');
+// Routes Imports
+const mainRoutes = require("./src/routes/main.routes");
+const adminRoutes = require("./src/routes/admin.routes");
+const authRoutes = require("./src/routes/auth.routes");
+const shopRoutes = require("./src/routes/shop.routes");
 // --------------
 
 const PORT = 3008;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // La siguiente app.set es para indicarle al motor ejs que las vistas van a estar en la carpeta esta
-app.set('views', './src/views');
+app.set("views", "./src/views");
 
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+app.use(express.static(__dirname + "/public"));
 
 // Configuramos las routes importadas para utilizarlos con middleware
-app.use('/', mainRoutes);
-app.use('/admin', adminRoutes);
-app.use('/auth', authRoutes);
-app.use('/shop', shopRoutes);
+app.use("/", mainRoutes);
+app.use("/admin", adminRoutes);
+app.use("/auth", authRoutes);
+app.use("/shop", shopRoutes);
 
 // Error 404
 app.use((req, res, next) => {
@@ -39,4 +42,6 @@ app.use((req, res, next) => {
 });
 // --------------
 
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+);
