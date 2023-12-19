d00db6b961aa168e770fc6e80b356a2615213711
maxiderecho <maxi.derecho@gmail.com>
2023-12-18 21:23:06 -0300

    preparando validaciones y repo para subir a vercel. agrego env.

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'mysql-maxiderecho.alwaysdata.net',
-    user: '339478_grupo15',
-    password: 'grupo15cac',
-    port: 3306,
-    database: 'maxiderecho_cac_grupo15',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.PORT,
+    database: process.env.DB_NAME,
     connectionLimit: 10,
     waitForConnections: true,
     queueLimit: 0
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { body } = require('express-validator');
+
 // Importamos el controlador auth.controller.js y agregamos ruta al router.get, post, etc
 
 const controller = require('../controllers/auth.controller');
