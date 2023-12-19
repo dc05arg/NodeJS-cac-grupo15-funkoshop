433873d1721677970760fd05b44e0bd13fee55fa
maxiderecho <maxi.derecho@gmail.com>
2023-12-18 23:05:24 -0300

    Agrego validaciones en login, create y edit.

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,9 +3,37 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/uploadFiles');
+
+const validateInput = require('../middlewares/validator');
+const { body } = require('express-validator');
+
 // Importamos el controlador admin.controller.js y agregamos ruta al router.get
 
 const controller = require('../controllers/admin.controller');
+
+const createValidation = [
+    body('categoria')
+        .notEmpty()
+        .withMessage('Seleccione la categoría del producto'),
+    body('licencia')
+        .notEmpty()
+        .withMessage('Seleccione la licencia del producto'),
+    body('nombre')
+        .notEmpty()
+        .withMessage('Ingrese el nombre del producto'),
+    body('descripcion')
+        .notEmpty()
+        .withMessage('Ingrese la descripción del producto'),
+    body('sku')
+        .notEmpty()
+        .withMessage('Ingrese el SKU del producto'),
+    body('precio')
+        .notEmpty()
+        .withMessage('Ingrese el precio del producto'),
+    body('stock')
+        .notEmpty()
+        .withMessage('Ingrese el stock del producto'),
+];
 
 const isLogged = (req, res, next) => {
     if (req.session.isLogged) {
@@ -17,9 +45,9 @@ const isLogged = (req, res, next) => {
 
 router.get('/', isLogged, controller.admin);
 router.get('/create', isLogged, controller.createView);
-router.post('/create', isLogged, upload.array('imagenes', 2), controller.createItem);
+router.post('/create', isLogged, upload.array('imagenes', 2), createValidation, validateInput, controller.createItem);
 router.get('/edit/:id', isLogged, controller.editView);
-router.put('/edit/:id', isLogged, upload.array('imagenes', 2), controller.editItem);
+router.put('/edit/:id', isLogged, upload.array('imagenes', 2), createValidation, validateInput, controller.editItem);
 router.delete('/delete/:id', isLogged, controller.deleteItem);
 
 // Exportación
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,14 +3,25 @@
 const express = require('express');
 const router = express.Router();
 
+
+const validateInput = require('../middlewares/validator');
 const { body } = require('express-validator');
 
 // Importamos el controlador auth.controller.js y agregamos ruta al router.get, post, etc
 
 const controller = require('../controllers/auth.controller');
 
+const loginValidation = [
+    body('email')
+        .isEmail()
+        .withMessage('Ingrese un correo válido'),
+    body('password')
+        .isLength({min:4})
+        .withMessage('La contraseña debe contener al menos 4 caracteres.'),
+];
+
 router.get('/login', controller.login);
-router.post('/login', controller.doLogin);
+router.post('/login', loginValidation, validateInput, controller.doLogin);
 router.get('/register', controller.register);
 router.post('/register', controller.doRegister);
 router.get('/logout', controller.logout);
